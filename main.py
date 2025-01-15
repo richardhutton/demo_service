5fe7cf549c655d2705b7fc15df702b26f92f1d11
Richard Hutton <rich.hutton@googlemail.com>
2025-01-15 16:54:31 +0100

    added health check

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "You suck"}
+  return {"message": "You suck"}
+
+
+@app.get("/health")
+def health_check():  
+  return {"message": "Health"}
